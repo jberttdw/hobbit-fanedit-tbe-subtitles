8ee3cd4b90f10dcc901909da488121b391c67533
JBert <jberttdw@gmail.com>
2020-01-10 01:16:40 +0100

    Add a script to help with subtitle renumbering
    SubtitleEdit has a tool for this, but then each file needs to be opened
    by hand before the tool can be used.

---
+++ scripts/renumber-indexes.ps1
@@ -0,0 +1,28 @@
+﻿# Picks up all srt files in the current directory, opens each one and rnumbers all
+# subtitles so the numbering in each file starts from 1
+
+function Convert-SubRipFile {
+    param (
+        $file
+    )
+    $lines = Get-Content -Encoding UTF8 $file
+    $number = 1
+    $nextRealLineIsNumber = $true
+    foreach ($line in $lines)
+    {
+        if ([string]::IsNullOrWhiteSpace($line)) {
+            Write-Output $line
+            $nextRealLineIsNumber = $true
+        } elseif ($nextRealLineIsNumber) {
+            Write-Output $number
+            $number++
+            $nextRealLineIsNumber = $false
+        } else {
+            Write-Output $line
+        }
+    }
+}
+
+$subtitleFiles = Get-ChildItem "*.srt"
+
+$subtitleFiles | % { Convert-SubRipFile $_ | Set-Content -Encoding UTF8 $_ ; Write-Host "Updated file $_" }
